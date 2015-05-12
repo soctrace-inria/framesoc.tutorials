d1e1aa36ea296ad4bb6e4886defc0ca9ce0543fc
Generoso Pagano <generoso.pagano@inria.fr>
2015-05-12 17:29:04 +0200

    Empty framesoc tool

---
+++ fr.inria.soctrace.framesoc.tutorials.emptytool/src/fr/inria/soctrace/framesoc/tutorials/emptytool/FramesocTestTool.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package fr.inria.soctrace.framesoc.tutorials.emptytool;
+
+import fr.inria.soctrace.framesoc.core.tools.model.FramesocTool;
+import fr.inria.soctrace.framesoc.core.tools.model.IFramesocToolInput;
+
+/**
+ * Empty Framesoc tool.
+ * 
+ * The tool can be launched by using the Framesoc menu. This class can be used
+ * as a skeleton for testing some code proposed in other Framesoc tutorials. The
+ * {@link #launch(IFramesocToolInput)} method can be used to this purpose.
+ * 
+ * @author "Generoso Pagano <generoso.pagano@inria.fr>"
+ */
+public class FramesocTestTool extends FramesocTool {
+
+	@Override
+	public void launch(IFramesocToolInput input) {
+
+		// TODO put your code here
+
+	}
+
+}
