09ae5fe1ecc080151e011ad7f15be3dc2e673df7
Generoso Pagano <generoso.pagano@inria.fr>
2015-05-13 15:48:20 +0200

    fix

--- fr.inria.soctrace.framesoc.tutorials.query/src/fr/inria/soctrace/framesoc/tutorials/query/QueryTool.java
+++ fr.inria.soctrace.framesoc.tutorials.query/src/fr/inria/soctrace/framesoc/tutorials/query/QueryTool.java
@@ -11,13 +11,9 @@ import fr.inria.soctrace.framesoc.core.tools.model.IFramesocToolInput;
  */
 public class QueryTool extends FramesocTool {
 	
-	/* (non-Javadoc)
-	 * @see fr.inria.soctrace.framesoc.core.tools.model.FramesocTool#launch(fr.inria.soctrace.framesoc.core.tools.model.IFramesocToolInput)
-	 */
 	@Override
 	public void launch(IFramesocToolInput input) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
