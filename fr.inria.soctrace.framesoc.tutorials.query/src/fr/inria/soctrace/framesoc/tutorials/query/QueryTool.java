8f2f7aeb30f9b8c4fb159ae465ed67d617584d32
Generoso Pagano <generoso.pagano@inria.fr>
2015-05-13 10:55:18 +0200

    starting query tutorial

---
+++ fr.inria.soctrace.framesoc.tutorials.query/src/fr/inria/soctrace/framesoc/tutorials/query/QueryTool.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package fr.inria.soctrace.framesoc.tutorials.query;
+
+import fr.inria.soctrace.framesoc.core.tools.model.FramesocTool;
+import fr.inria.soctrace.framesoc.core.tools.model.IFramesocToolInput;
+
+/**
+ * @author "Generoso Pagano <generoso.pagano@inria.fr>"
+ */
+public class QueryTool extends FramesocTool {
+	
+	/* (non-Javadoc)
+	 * @see fr.inria.soctrace.framesoc.core.tools.model.FramesocTool#launch(fr.inria.soctrace.framesoc.core.tools.model.IFramesocToolInput)
+	 */
+	@Override
+	public void launch(IFramesocToolInput input) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
